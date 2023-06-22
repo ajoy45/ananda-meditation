d2159ef038de0e383ab85e71f29010688249b7fb
ajoy45 <ajoypust@gmail.com>
2023-06-22 12:10:34 +0600

    banner added

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,14 +1,11 @@
 import React, { useEffect, useState } from 'react';
-import slider1 from '../../../src/assets/slider/slider1.mp4'
-import slider2 from '../../../src/assets/slider/slider2.mp4'
-import slider3 from '../../../src/assets/slider/slider3.mp4'
-import { Swiper, SwiperSlide } from "swiper/react";
-import "swiper/css";
-import "swiper/css/navigation";
-import { Navigation } from "swiper";
+
+
+
 import PopularInstructor from './PopularInstructor';
 import PopularClass from './popularClass';
 import Step from './Step';
+import Banner from './Banner';
 
 
 const Home = () => {
@@ -17,15 +14,10 @@ const Home = () => {
         const mode=localStorage.getItem('mode')
          setTheme(mode)
     },[localStorage.getItem('mode')])
-    console.log(theme)
+    // console.log(theme)
     return (
         <div className=''>
-            <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
-                <SwiperSlide><video controls autoPlay loop muted src={slider1}></video></SwiperSlide>
-                <SwiperSlide><video controls autoPlay loop muted src={slider2}></video></SwiperSlide>
-                <SwiperSlide><video controls autoPlay loop muted src={slider3}></video></SwiperSlide>
-               
-            </Swiper>
+            <Banner></Banner>
             <div>
                 <PopularClass></PopularClass>
             </div>
